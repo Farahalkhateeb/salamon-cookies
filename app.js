59784083da99f07224d721d829ec72532faa4db1
Farahalkhateeb <farahkhalid96@yahoo.comclear>
2021-06-28 23:45:42 +0300

    new

--- app.js
+++ app.js
@@ -1,40 +1,49 @@
 'use strict';
+let hours=['9am' , '10am' , '11am', '12pm' , '1pm' , '2pm' , '3pm' ,'4pm' , '5pm' ,'6pm' , '7pm'];
+
+
+
 let Seattle = {
-    location: "Seattle1",
-    min: 23,
-    max: 65,
-    avg: 6.3,
-    hours:['9am' , '10am' , '11am', '12pm' , '1pm' , '2pm' , '3pm' ,'4pm' , '5pm' ,'6pm' , '7pm'], 
-    CustomerPerHour: function (minimum,maximum) {
-        return Math.floor(Math.random() * (maximum - minimum +1) + minimum);
 
+    shopName:'seattle',
+    mincus: 23,
+    maxcus: 65,
+    avgCookies: 6.3,
+    randcust:[],
+    avgcookiesph:[],
+    calcrandCustomerPerHour: function (){
+
+
+     let min = Math.ceil(this.mincus);
+       let max = Math.floor(this.maxcus);
+        let randcust=Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+      
+        for(let i=0;i<hours.length;i++){
+
+
+            let min = Math.ceil(this.mincus);
+            let max = Math.floor(this.maxcus);
+             let randcust=Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+
+
+
+            this.randcust.push();
+
+        }
     },
-    TotalCookiesperHour: [],
+    calAvgCookiesPerHour: function(){
+        for(let i=0;i<hours.length;i++){
 
-    totalcookies : 0,
+            this.avgcookiesph[i]=Math.floor?(this.randcust[i]*this.avgCookies)
 
-render : function(){
-    let personelemantary = document.getElementById('personelemantary');
-    let ulelemantary = document.createElement('ulElemantary');
-    ulelemantary.textContent ="seattle";
-    personelemantary.appendChild(ulelemantary);
 
-    for(let i=0; i<hours.length; i++){
-        this. TotalCookiesperHou.push(Math.floor(this.CustomerPerHour(this.minimum,this .maximum) *  this.avg ));
+        }
 
     }
 
-for(let i =0; i<TotalCookiesperHour.length; i++){
-    let lielemantary = document.createElement('li')
-    window.li2=lielemantary;
-    lielemantary.textContent= hours(i)+':'+this.TotalCookiesperHour[i];
-    ulelemantary.appendChild(lielemantary);
-    this.totalcookies= this.totalcookies + this.TotalCookiesperHour[i];
-    console.log(this.totalcookies);
-
+    
 }
-}
+console.log(Seattle);
 
-}
-
-Seattle.render();
+Seattle.calcrandCustomerPerHour();
+Seattle.calAvgCookiesPerHour();
